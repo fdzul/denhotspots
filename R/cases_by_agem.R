f190c8f85becb10241856e99588881210b302902
Felipe Dzul <felipe.dzul.m@gmail.com>
2020-12-11 17:29:22 -0600

    This commit add the function and document the functions.

---
+++ R/cases_by_agem.R
@@ -0,0 +1,126 @@
+#' cases_by_agem
+#'
+#' this function left joint sf AGEM and the cases by municipality.
+#'
+#' @param path is the directory where the txt of dengue file exist.
+#' @param disease  is a string for define the vector borne disease. The option are "Paludismo", "Dengue" & "covid.
+#'
+#' @return a sf object. The municipality con de cases by year.
+#' @export
+#'
+#' @examples 1+1 
+cases_by_agem <- function(path, disease){
+    
+    # step 1.1 Define the columns ####
+    if(disease == "Paludismo"){
+        col_names <- c("FOL_ID", "VEC_ID", "IDE_SEX", 
+                       "CVE_EDO_RES", "DES_EDO_RES",
+                       "CVE_JUR_RES", "DES_JUR_RES",
+                       "CVE_MPO_RES", "DES_MPO_RES",
+                       "CVE_LOC_RES", "DES_LOC_RES",
+                       "IDE_CALLE1", "IDE_CALLE2", "IDE_CP",
+                       "CVE_DIAG_PROBABLE", "DES_DIAG_PROBABLE",
+                       "CVE_DIAG_FINAL", "DES_DIAG_FINAL",
+                       "FEC_INI_SIGNOS_SINT", "ANO", "SEM", 
+                       "FIEBRE", "FEC_INI_FIEBRE", "FEC_INI_SIGNOS_ALARMA",
+                       "MANEJO", "FEC_INGRESO", "EVOLUCION",
+                       "ESPECIE_PALUDISMO",
+                       "TRATAMIENTO_PALUDISMO", "FEC_INI_TRAT_PAL", "FEC_FIN_TRAT_PAL",
+                       "MED_TRAT_PAL", "MED_TRAT_PAL_1", "MED_TRAT_PAL_2")
+    }
+    if(disease == "Dengue"){
+        col_names <- c("IDE_EDA_ANO", "IDE_SEX",
+                       "IDE_CAL", "NUM_EXT", "NUM_INT",
+                       "IDE_COL", "IDE_CP",
+                       "CVE_LOC_RES", "DES_LOC_RES", "CVE_MPO_RES", "DES_MPO_RES",
+                       "DES_JUR_RES", 
+                       "CVE_EDO_RES", "DES_EDO_RES",
+                       "ESTATUS_CASO", "CVE_DIAG_PROBABLE", "DES_DIAG_PROBABLE", "DES_DIAG_FINAL",
+                       "FEC_INI_SIGNOS_SINT", "ANO", "SEM",
+                       "RESULTADO_NS1", "RESULTADO_IGM", "RESULTADO_IGG",
+                       "RESULTADO_PCR", "RESULTADO_IGMC", "RESULTADO_MAC",
+                       "MANEJO")
+    }
+    # Step 1.2. read the files with datatable ####
+    if(disease == "covid"){
+        y <- data.table::fread(path, header = TRUE) %>%
+            dplyr::filter(RESULTADO_LAB == 1) %>%
+            dplyr::mutate(date = lubridate::ymd(FECHA_SINTOMAS),
+                          week = lubridate::week(date)) %>%
+            dplyr::group_by(week, ENTIDAD_RES, MUNICIPIO_RES) %>%
+            dplyr::summarise(n = dplyr::n(),  .groups = "drop") %>%
+            tidyr::pivot_wider(names_from = week,
+                               values_from = n,
+                               values_fill = list(n = 0)) %>%
+            dplyr::mutate(CVE_EDO_RES = stringr::str_pad(ENTIDAD_RES, 
+                                                         pad = "0", 
+                                                         side = "left", 
+                                                         width = 2),
+                          CVE_MPO_RES = stringr::str_pad(MUNICIPIO_RES, 
+                                                         pad = "0", 
+                                                         side = "left", 
+                                                         width = 3),
+                          ENTIDAD_RES = NULL,
+                          MUNICIPIO_RES = NULL) %>%
+            as.data.frame() 
+    } else {
+        
+        y <- purrr::map_dfr(.x = list.files(path = path,
+                                            full.names = TRUE,
+                                            pattern = "txt"), 
+                            .f = data.table::fread, 
+                            stringsAsFactors = FALSE,
+                            select = col_names,
+                            quote = "",
+                            fill = TRUE) %>%
+            dplyr::filter(DES_DIAG_FINAL == "PALUDISMO") %>%
+            dplyr::filter(!DES_EDO_RES %in% c("OTROS PAISES DE LATINOAMERICA",
+                                              "ESTADOS UNIDOS DE NORTEAMERICA",
+                                              "OTROS PAISES")) %>%
+            dplyr::group_by(CVE_EDO_RES, CVE_MPO_RES, ANO) %>%
+            dplyr::summarise(n = dplyr::n(), .groups = "drop") %>%
+            tidyr::pivot_wider(names_from = ANO,
+                               values_from = n,
+                               values_fill = list(n = 0)) %>%
+            dplyr::mutate(CVE_EDO_RES = stringr::str_pad(CVE_EDO_RES, 
+                                                         pad = "0", 
+                                                         side = "left", 
+                                                         width = 2),
+                          CVE_MPO_RES = stringr::str_pad(CVE_MPO_RES, 
+                                                         pad = "0", 
+                                                         side = "left", 
+                                                         width = 3)) %>%
+            as.data.frame()   
+    }
+    
+    
+    # step 3. load the AGEM of inegi 2019 ####
+    x <- rgeomex::AGEM_inegi19_mx
+    
+    # step 4. left joint between aggregated data and AGEM ######
+    xy <- dplyr::left_join(x = x,
+                           y = y,
+                           by = c("CVE_ENT"  = "CVE_EDO_RES",
+                                  "CVE_MUN"  = "CVE_MPO_RES"))
+    
+    # step 5. sustitute the NA values by 0 ####
+    xy[is.na(xy)] <- 0
+    
+    # step 6. set column names ####
+    if(disease == "Paludismo"){
+        names(xy)[-c(1:4,ncol(xy))] <- paste("PAL", 
+                                             names(xy)[-c(1:4,ncol(xy))], sep = "_")
+    }
+    
+    if(disease == "Dengue"){
+        names(xy)[-c(1:4,ncol(xy))] <- paste("DENV", 
+                                             names(xy)[-c(1:4,ncol(xy))], sep = "_")
+    }
+    if(disease == "covid"){
+        names(xy)[-c(1:4,ncol(xy))] <- paste("COVID", 
+                                             names(xy)[-c(1:4,ncol(xy))], sep = "_")
+    }
+    
+    # step 7. return the result ####
+    xy 
+}
---
+++ R/intensity_plot.R
@@ -0,0 +1,68 @@
+#' intensuty_plot
+#' 
+#' this function plot the event hotspots by polygon.
+#'
+#' @param x is the hotspots dataset.
+#' @param pal is the palette.
+#' @param option is the palette option. 
+#' @param name is the palette name. the option and name are very similar, some package use name (ej. rcartocolor::carto_pal) and other option (ex. viridis:viridis)
+#' @param pal_name is a logical value, if pal_name TRUE is for packages that use name, else the packages that use option.
+#' @param breaks is a numeric value. Is the increment of the sequence.
+#' @param dir_pal is the direction of the palette, 1 forward, -1 reverse.
+#' @param x_leg is the x coordinates of legend.
+#' @param y_leg is the x coordinates of legend.
+#'
+#' @return a ggplot
+#' @export
+#'
+#' @examples 1+1 
+intensity_plot <- function(x, pal, option = NULL, name = NULL, pal_name, breaks, dir_pal = NULL, x_leg, y_leg){
+    if(pal_name == TRUE){
+        ggplot2::ggplot()+
+            ggplot2::geom_sf(data = x,
+                             fill = "grey88",
+                             col = "white",
+                             lwd = 0.1) +
+            ggplot2::theme_void() +
+            ggplot2::geom_sf(data = x %>% dplyr::filter(intensity_gi > 0),
+                             ggplot2::aes(fill = intensity_gi),
+                             col = "gray95",
+                             lwd = 0.1) +
+            ggplot2::scale_fill_gradientn(name = "Intensidad",
+                                          colours = c(pal(n = max(x$intensity_gi),
+                                                          name = name)),
+                                          breaks = seq(from = 1, 
+                                                       to = max(x$intensity_gi),
+                                                       by = breaks),
+                                          guide = "legend") +
+            ggplot2::geom_sf(data = rgeomex::AGEE_inegi19_mx,
+                             fill = NA,
+                             col = "black",
+                             lwd = 1) +
+            ggplot2::theme(legend.position = c(x_leg, y_leg))
+    } else{
+        ggplot2::ggplot()+
+            ggplot2::geom_sf(data = x,
+                             fill = "grey95",
+                             col = "white",
+                             lwd = 0.1) +
+            ggplot2::theme_void() +
+            ggplot2::geom_sf(data = x %>% dplyr::filter(intensity_gi > 0),
+                             ggplot2::aes(fill = intensity_gi),
+                             col = "gray90",
+                             lwd = 0.1) +
+            ggplot2::scale_fill_gradientn(name = "Intensidad",
+                                          colours = c(pal(n = max(x$intensity_gi),
+                                                          option = option,
+                                                          direction = dir_pal)),
+                                          breaks = seq(from = 1, 
+                                                       to = max(x$intensity_gi),
+                                                       by = breaks),
+                                          guide = "legend") +
+            ggplot2::geom_sf(data = rgeomex::AGEE_inegi19_mx,
+                             fill = NA,
+                             col = "black",
+                             lwd = 1) +
+            ggplot2::theme(legend.position = c(x_leg, y_leg))
+    }
+}
