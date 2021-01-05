f5cf970434b488235e0bda8f27dda39df762b1d9
Felipe Dzul <felipe.dzul.m@gmail.com>
2021-01-05 15:08:12 -0600

    this commit update the intensity_plot

--- R/intensity_plot.R
+++ R/intensity_plot.R
@@ -1,10 +1,10 @@
-#' intensuty_plot
-#' 
+#' intensity_plot
+#'
 #' this function plot the event hotspots by polygon.
 #'
 #' @param x is the hotspots dataset.
 #' @param pal is the palette.
-#' @param option is the palette option. 
+#' @param option is the palette option.
 #' @param name is the palette name. the option and name are very similar, some package use name (ej. rcartocolor::carto_pal) and other option (ex. viridis:viridis)
 #' @param pal_name is a logical value, if pal_name TRUE is for packages that use name, else the packages that use option.
 #' @param breaks is a numeric value. Is the increment of the sequence.
@@ -15,7 +15,7 @@
 #' @return a ggplot
 #' @export
 #'
-#' @examples 1+1 
+#' @examples 1+1
 intensity_plot <- function(x, pal, option = NULL, name = NULL, pal_name, breaks, dir_pal = NULL, x_leg, y_leg){
     if(pal_name == TRUE){
         ggplot2::ggplot()+
@@ -31,7 +31,7 @@ intensity_plot <- function(x, pal, option = NULL, name = NULL, pal_name, breaks,
             ggplot2::scale_fill_gradientn(name = "Intensidad",
                                           colours = c(pal(n = max(x$intensity_gi),
                                                           name = name)),
-                                          breaks = seq(from = 1, 
+                                          breaks = seq(from = 1,
                                                        to = max(x$intensity_gi),
                                                        by = breaks),
                                           guide = "legend") +
@@ -55,7 +55,7 @@ intensity_plot <- function(x, pal, option = NULL, name = NULL, pal_name, breaks,
                                           colours = c(pal(n = max(x$intensity_gi),
                                                           option = option,
                                                           direction = dir_pal)),
-                                          breaks = seq(from = 1, 
+                                          breaks = seq(from = 1,
                                                        to = max(x$intensity_gi),
                                                        by = breaks),
                                           guide = "legend") +
