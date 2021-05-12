2a53dc7a752abe6ff866b9b6961d21eda93afc51
Felipe Dzul <felipe.dzul.m@gmail.com>
2021-05-12 13:11:05 -0500

    This commit add the power_laws data and update the package

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' Power Laws data.
+#'
+#' A dataset containing the Power Laws dataset.
+#'
+#' @format A dataframe object with 606 rows and 3 variables:
+#' \describe{
+#'   \item{y}{are the percentiles of the y variable }
+#'   \item{x}{are the percentiles of the x variable}
+#'   \item{power_law}{are the power distributions}
+#'   ...
+#' }
+#' @source \url{https://www.tandfonline.com/doi/abs/10.1080/00207390802213609}
+"power_laws"
